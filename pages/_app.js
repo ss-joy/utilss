e317e85e1e9e55bf787633bf91c0488e54f1c38a
ss joy <ssjoy.u.b@gmail.com>
2023-09-23 23:57:05 +0600

    add head metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import MainHeader from "@/components/Home/MainHeader";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>utilss</title>
+      </Head>
       <MainHeader />
       <Component {...pageProps} />
     </>
