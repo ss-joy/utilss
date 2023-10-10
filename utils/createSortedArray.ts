41abda2bb010e3535d2f5acd4cb441a91a61acdf
ss joy <ssjoy.u.b@gmail.com>
2023-10-11 00:44:00 +0600

    fix: array now includes the endvalue

--- utils/createSortedArray.ts
+++ utils/createSortedArray.ts
@@ -1,10 +1,10 @@
 //range: upto 90000000 element(s)
 export default function createSortedArray(
   startValue: number,
-  endValue: number
+  endValue: number,
 ) {
   const tempArray = [];
-  for (let i = startValue; i < endValue; i++) {
+  for (let i = startValue; i <= endValue; i++) {
     tempArray.push(i);
   }
   return tempArray;
